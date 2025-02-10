e6b432640e8349e12a36f13f80042992a4b9db78
Angel Angeles <angel.angeles.molina@gmail.com>
2025-02-09 18:03:18 -0600

    article Fragment

--- app/src/main/java/com/example/newsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/ArticleFragment.kt
@@ -5,36 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
 import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentArticleBinding
+import com.example.newsapp.ui.NewsActivity
+import com.example.newsapp.ui.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
 
-class ArticleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class ArticleFragment : Fragment(R.layout.fragment_article) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
+    lateinit var newsViewModel: NewsViewModel
+    val args: ArticleFragmentArgs by navArgs()
+    lateinit var binding: FragmentArticleBinding
 
-        }
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentArticleBinding.bind(view)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false)
-    }
 
-    companion object {
-
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ArticleFragment().apply {
-                arguments = Bundle().apply {
-
-                }
+        newsViewModel = (activity as NewsActivity).newsViewModel
+        val article = args.article
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            article.url?.let {
+                loadUrl(it)
             }
+        }
+
+        binding.fab.setOnClickListener {
+            newsViewModel.addToFavourites(article)
+            Snackbar.make(view, "Added to favourites", Snackbar.LENGTH_SHORT).show()
+        }
+
     }
+
 }
